ad3cdf0e016b05543001189705cd0a44c693d3c7
drmattcrooks <drmattcrooks@gmail.com>
2023-02-02 09:35:50 +0000

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='matly',
-    version='0.1',
+    version='0.2',
     license='',
     description='matplotlib syntax for plotly',
     packages=find_packages('src'),
