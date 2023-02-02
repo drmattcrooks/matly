292922a7769a9ef59574b2492fa78fafee9b7be9
drmattcrooks <drmattcrooks@gmail.com>
2023-02-02 09:33:56 +0000

    basic plot functionality

--- src/matly/matly_class.py
+++ src/matly/matly_class.py
@@ -6,7 +6,40 @@ import os
 
 class Matly:
     def __init__(self, *args, **kwargs):
-        pass
+        try:
+            self.fig = kwargs['figure']
+        except:
+            raise KeyError("No figure handle passed when initiating Matly class")
+
+
+    def plot(self, *args):
+        kwargs = {
+            'x': None,
+            'y': None
+        }
+        if len(args) == 1:
+            kwargs['y'] = args[0]
+            kwargs['x'] = list(range(len(args[0])))
+        elif len(args) == 2:
+            kwargs['x'] = args[0]
+            kwargs['y'] = args[1]
+        self._plot(**kwargs)
+
+    def _plot(self, **kwargs):
+        plot_trace = self._get_plot_defaults()
+        plot_style_dict = {'line': dict(),
+                           'marker': {'line': dict()},
+                           'marker_symbol': None,
+                           'mode': 'lines'}
+        plot_trace.update(
+            x = kwargs['x'],
+            y = kwargs['y'],
+            **plot_style_dict
+        )
+        self.fig.add_trace(plot_trace)
+
+    def _get_plot_defaults(self):
+        return go.Scatter()
 
 class figureHandle(go.Figure):
     def __init__(self, *args, **kwargs):
